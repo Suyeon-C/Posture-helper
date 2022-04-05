86814dfe2d8bfa665db96e926a6a19183fdb61db
Suyeon-C <santanoel1225@naver.com>
2022-04-05 22:18:22 +0900

    first commit

---
+++ Posture.py
@@ -0,0 +1,118 @@
+import cv2
+import tensorflow
+from PIL import Image, ImageOps
+import numpy as np
+import beepy
+import kakao_utils
+
+# 내장 카메라 사용 :0
+capture = cv2.VideoCapture(0)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+
+
+def beepsound():
+    beepy.beep(sound=6)
+
+# 카카오톡 메세지에 담을 내용 
+def send_music_link():
+    KAKAO_TOKEN_FILENAME = "kakao_message/kakao_token.json"
+    KAKAO_APP_KEY = "dab7b2668963c716574013798831227d"
+    tokens = kakao_utils.update_tokens(KAKAO_APP_KEY, KAKAO_TOKEN_FILENAME)
+
+    template = {
+        "object_type": "text",
+        "text": "자세가 틀렸어요! 아래 영상을 보고 다시 해보세요",
+        "link": {
+            "web_url": "https://www.youtube.com/watch?v=IfJcq4LDXKE",
+            "mobile_web_url": "https://www.youtube.com/watch?v=IfJcq4LDXKE"
+        },
+        "button_title": "바른 자세 확인하기"
+    }
+
+    # 카카오톡 메시지 전송
+    res = kakao_utils.send_message(KAKAO_TOKEN_FILENAME, template)
+    if res.json().get('result_code') == 0:
+        print('메시지를 성공적으로 보냈습니다.')
+    else:
+        print('메시지를 보내지 못했습니다. 오류메시지 : ', res.json())
+
+
+# 이미지 전처리 함수
+def preprocessing(frame):
+    size = (224,224)
+    frame_resized = cv2.resize(frame, size, interpolation=cv2.INTER_AREA)
+    frame_normalized = (frame_resized.astype(np.float32) / 127.0) - 1
+    frame_reshaped = frame_normalized.reshape((1, 224, 224, 3))
+
+    return frame_reshaped
+
+
+# Load the model
+model = tensorflow.keras.models.load_model('keras_model.h5')
+
+# Create the array of the right shape to feed into the keras model
+# The 'length' or number of images you can put into the array is
+# determined by the first position in the shape tuple, in this case 1.
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+# Replace this with the path to your image
+image = Image.open('kakao_message/wrong.jpg')
+
+#resize the image to a 224x224 with the same strategy as in TM2:
+#resizing the image to be at least 224x224 and then cropping from the center
+size = (224, 224)
+image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+#turn the image into a numpy array
+image_array = np.asarray(image)
+
+# display the resized image
+image.show()
+
+# Normalize the image
+normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+# Load the image into the array
+data[0] = normalized_image_array
+
+# run the inference
+prediction = model.predict(data)
+print(prediction)
+
+model_filename = 'keras_model.h5'
+model = tensorflow.keras.models.load_model(model_filename)
+
+cnt = 1  
+while True:
+    ret, frame = capture.read()
+    if ret == True:
+        print("보고있습니다")
+
+    frame_fliped = cv2.flip(frame,1) 
+    cv2.imshow("VideoFrame", frame_fliped)
+
+    if cv2.waitKey(200) > 0:
+        break
+
+    # 데이터 전처리
+    preprocessed = preprocessing(frame_fliped)
+    prediction = model.predict(preprocessed)
+        
+    if prediction[0,0] < prediction[0,1]:
+        print('잘못된 자세')
+        cnt += 1
+
+        # 잘못된 자세로 1분간 운동 시, 알람소리 & 카카오톡 보내기
+        if cnt % 30 == 0:
+            cnt = 1
+            print('잘못된 자세 입니다')
+            beepsound()
+            send_music_link()
+            break # 1번만 알람이 오면 프로그램을 정지시킴(반복을 원한다면, 주석으로 막기)
+    else:
+        print('좋은 자세 입니다.')
+        cnt = 1
+
+capture.release()
+cv2.destroyAllWindows
---
+++ kakao_utils.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python 
+# coding: utf-8
+
+import json
+import os
+import datetime
+import requests
+from sqlparse import tokens
+
+KAKAO_TOKEN_FILENAME = "kakao_message/kakao_token.json"
+
+# 저장하는 함수
+def save_tokens(filename, tokens):
+    with open(filename, "w") as fp:
+        json.dump(tokens, fp)
+
+# 읽어오는 함수
+def load_tokens(filename):
+    with open(filename) as fp:
+        tokens = json.load(fp)
+
+    return tokens
+
+# refresh_token으로 access_token 갱신하는 함수
+def update_tokens(app_key, filename) :
+    tokens = load_tokens(filename)
+
+    url = "https://kauth.kakao.com/oauth/token"
+    data = {
+        "grant_type" : "refresh_token",
+        "client_id" : app_key,
+        "refresh_token" : tokens['refresh_token']
+    }
+    response = requests.post(url, data=data)
+
+    # 요청에 실패했다면,
+    if response.status_code != 200:
+        print("error! because ", response.json())
+        tokens = None
+    else: # 성공했다면,
+        print(response.json())
+        # 기존 파일 백업
+        now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        backup_filename = filename+"."+ now
+        os.rename(filename, backup_filename)
+        # 갱신된 토큰 저장
+        tokens['access_token'] = response.json()['access_token']
+        save_tokens(filename, tokens)
+        
+    return tokens
+
+url = "https://kauth.kakao.com/oauth/token"
+
+data = {
+    "grant_type" : "authorization_code",
+    "client_id" : "dab7b2668963c716574013798831227d",
+    "redirect_uri" : "https://localhost.com",
+    "code" : "5tEFATGvWObsZaY4ifRvekndLpZdeGa_WFCjcRiL3wkw1Y6Ebsh1wCHhExZL3UOLjSJNrwo9dNsAAAF_-ZTCmA" # code=xxx, xxx 부분 적기 https://kauth.kakao.com/oauth/authorize?client_id=rest id 적기&response_type=code&redirect_uri=https://localhost.com
+}
+
+response = requests.post(url, data=data)
+
+# 요청에 실패한다면,
+if response.status_code != 200:
+    print("error ! because" , response.json())
+else:
+    tokens = response.json()
+    print(tokens)
+
+    # 토큰 저장
+    save_tokens(KAKAO_TOKEN_FILENAME, tokens)
+
+# 메세지 전송 함수 
+def send_message(filename, template):
+    tokens = load_tokens(filename)
+
+    headers = {
+        "Authorization" : "Bearer " + tokens['access_token']
+    }    
+    #Json 형식 => 문자열
+    payload = {
+        "template_object" : json.dumps(template)
+    }
+
+    #카카오톡 보내기
+    url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
+    res = requests.post(url, data=payload, headers=headers)
+
+    return res
